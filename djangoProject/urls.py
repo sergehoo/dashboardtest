faa04370b0af7cd8f10e986156f224f0398b4696
ogahsergesergehoo <sergeleo14@yahoo.fr>
2025-09-28 20:38:28 +0000

    a

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -2,7 +2,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-
+from django.http import JsonResponse
 from dashboard import views
 def healthz(_):
     # ⚡ rapide : sans requête DB
